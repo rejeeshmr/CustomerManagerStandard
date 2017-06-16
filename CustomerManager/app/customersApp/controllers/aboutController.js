1d071cc54f94ccccb35ade0303d691b50a68477b
rradhakrishnan <rradhakrishnan@L-AH786611.etslan.org>
2017-06-15 20:58:45 -0400

    Commited

--- CustomerManager/app/customersApp/controllers/aboutController.js
+++ CustomerManager/app/customersApp/controllers/aboutController.js
@@ -3,11 +3,10 @@
     var injectParams = [];
 
     var AboutController = function () {
-
+  alert("Controller");
     };
 
     AboutController.$inject = injectParams;
-
     angular.module('customersApp').controller('AboutController', AboutController);
 
 }());
